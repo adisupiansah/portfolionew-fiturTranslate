7939521f40b7eefd9c10ef90d7e7bb7773f1f19e
adisupiansah <adisupiansah15@gmail.com>
2025-02-05 12:24:17 +0700

    Update new

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,65 @@
 'use client'
 import Image from "next/image";
-import React from "react";
+import React, {useState, useEffect} from "react";
 import foto from "/public/img/adi4.jpg";
 import { TypeAnimation } from "react-type-animation";
 import 'animate.css';
+import { useLanguange } from "@/context/TranslateContext";
+import { translateText } from "@/libs/Translate";
 
 const Home = () => {
+  const { language } = useLanguange();
+
+  const [translated, setTranslated] = useState({
+    paragraf1: "",
+    paragraf2: "",
+    paragraf31: "",
+    paragraf32: "",
+    paragraf4: ""
+  })
+
+const teksHome = [
+  {
+    id:1,
+    paragraf1: "Hallo Guys, I a'm",
+    paragraf2: "Adi Supiansah",
+    paragraf31: "Fullstack",
+    paragraf32: "Fullstack Developer",
+    paragraf4: "Welcome to My Personal website"
+  }
+]
+
+  const translatedAllText = async () => {
+    if (language === "id") {
+      try {
+        const translations = await Promise.all([
+          translateText(teksHome[0].paragraf1, "id"),
+          translateText(teksHome[0].paragraf2, "id"),
+          translateText(teksHome[0].paragraf31, "id"),
+          translateText(teksHome[0].paragraf32, "id"),
+          translateText(teksHome[0].paragraf4, "id")
+        ]);
+
+        setTranslated({
+          paragraf1: translations[0],
+          paragraf2: translations[1],
+          paragraf31: translations[2],
+          paragraf32: translations[3],
+          paragraf4: translations[4]
+        });
+      } catch (error) {
+        console.error("Error Translating Text Home:", error);
+        setTranslated(teksHome[0]);
+      }
+    } else {
+      setTranslated(teksHome[0])
+    }
+  }
+
+  useEffect(() => {
+    translatedAllText()
+  }, [language])
+
   return (
     <div className="home">
       <div className="container">
@@ -25,14 +79,14 @@ const Home = () => {
           {/* Teks */}
           <div className="col-md-4 order-2 order-md-1 text-center text-md-start animate__animated animate__slideInUp animate__fast">
             <div className="home-text">
-              <span className="txt-halo">Hallo Guys, I am</span>
-              <h1 className="txt-name">Adi Supiansah</h1>
+              <span className="txt-halo">{translated.paragraf1}</span>
+              <h1 className="txt-name">{translated.paragraf2}</h1>
               <h2 className="txt-job">
                 <TypeAnimation
                   sequence={[
-                    "Fullstack",
+                    translated.paragraf31,
                     500,
-                    "Fullstack Developer",
+                    translated.paragraf32,
                     500,
                     '',
                     500,
@@ -40,7 +94,7 @@ const Home = () => {
                   repeat={Infinity}
                 />
               </h2>
-              <span className="txt-desc">Welcome to My personal website</span>
+              <span className="txt-desc">{translated.paragraf4}</span>
             </div>
           </div>
         </div>
